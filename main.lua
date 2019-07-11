d8984cdf6f80bdec9498289d9cdd76f31b9b501b
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-07-12 04:16:59 +0530

    Initial setup with push library.

--- main.lua
+++ main.lua
@@ -1,20 +1,33 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,{
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
-    love.graphics.printf(
-        'Pong',
-        0,
-        WINDOW_HEIGHT/2 - 6,
-        WINDOW_WIDTH,
-        'center'
-    )
+
+    push:apply('start')
+
+    love.graphics.printf('Hello Pong', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    push:apply('end')
 end
