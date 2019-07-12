9979f8dfb1dd0d62e209110f71323e2fa02eacf7
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-07-12 05:55:41 +0530

    Added rectangles and font and ball.

--- main.lua
+++ main.lua
@@ -10,6 +10,11 @@ function love.load()
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+
+    font = love.graphics.newFont('ARCADECLASSIC.ttf', 48)
+
+    love.graphics.setFont(font)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{
         fullscreen = false,
         resizable = false,
@@ -27,7 +32,16 @@ function love.draw()
 
     push:apply('start')
 
-    love.graphics.printf('Hello Pong', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.clear(78 / 255, 94 / 255, 212 / 255, 255 / 255)
+
+    love.graphics.setColor(224 / 255, 201 / 255, 70 / 255, 255 / 255)
+
+    love.graphics.printf('WELCOME  TO  PONG', 0, 10, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setColor(255 / 255, 255 / 255, 255 / 255, 255 / 255)
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     push:apply('end')
 end
