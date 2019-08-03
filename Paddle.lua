16f6950a479ed1cfbde96af8859506bd7b61f722
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-03 16:46:56 +0530

    Added comments and indentation.

--- Paddle.lua
+++ Paddle.lua
@@ -1,5 +1,7 @@
+-- Initializing Paddle class
 Paddle = Class{}
 
+-- Constructor for paddle
 function Paddle:init(x, y, width, height, speed)
     self.x = x
     self.y = y
@@ -14,26 +16,37 @@ function Paddle:init(x, y, width, height, speed)
     self.score = 0
 end
 
-function Paddle:reset(x, y)
-    self.x = x
-    self.y = y
-    self.centerY = self.y + (self.height / 2)
-    self.score = 0
-end
-
+-- Function to update paddle according to move specified
 function Paddle:update(move, dt)
+
+    -- Moving up by required amount
     if move == "moveUp" then
         self.y = self.y - self.speed * dt
+    -- Moving down by required amount
     elseif move == "moveDown" then
         self.y = self.y + self.speed * dt
     end
 
+    -- Not letting paddle go below the bottom of screen
     if self.y >= 0 then
         self.y = math.min(self.y, VIRTUAL_HEIGHT - self.height)
+    -- Not letting paddle go above the top of screen
     else
         self.y = 0
     end
 
+    -- Updating centreY
     self.centerY = self.y + (self.height / 2)
 
+end
+
+-- Function to reset paddle
+function Paddle:reset(x, y)
+
+    -- Resetting required variables
+    self.x = x
+    self.y = y
+    self.centerY = self.y + (self.height / 2)
+    self.score = 0
+    
 end
