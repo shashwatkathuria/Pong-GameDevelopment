bdbc74fe558e8ad4c1be8647775f052f3fa8985e
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-03 16:41:03 +0530

    Added comments, indentation and refactored some code.

--- Ball.lua
+++ Ball.lua
@@ -1,6 +1,9 @@
+-- Initializing Ball class
 Ball = Class{}
 
+-- Constructor for ball
 function Ball:init(x, y, width, height)
+  
     self.x = x
     self.y = y
     self.width = width
@@ -11,50 +14,79 @@ function Ball:init(x, y, width, height)
 
 end
 
-function Ball:reset()
-
-    self.vx = math.random() > 0.5 and 150 or -150
-    self.vy = math.random(-300, 300)
-
-    self.x = VIRTUAL_WIDTH / 2 - 2
-    self.y = VIRTUAL_HEIGHT / 2 - 2
-end
-
-function Ball:updateOnCollision(player, paddle)
-    print(self.vy)
-    yOffsetVelocity = 1 + ( math.abs(self.y - paddle.centerY) / paddle.height ) / 2
-    print(yOffsetVelocity)
-    if player == "left" then
-        self.vy = self.vy * yOffsetVelocity
-        self.vx = -self.vx * 1.05
-        self.x = paddle.x + paddle.width
-    elseif player == "right" then
-        self.vy = self.vy * yOffsetVelocity
-        self.vx = -self.vx * 1.05
-        self.x = paddle.x - self.width
-
-    end
-end
-
-function Ball:hasCollided(paddle)
-    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
-      return false
-    end
-    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
-      return false
-    end
-
-    return true
-end
-
+-- Function to update ball and rebound if required
 function Ball:update(dt)
+
+    -- Rebounding on collision with top of screen
     if self.y < 0 then
         self.y = 0
         self.vy = -self.vy
+
+    -- Rebounding on collision with bottom of screen
     elseif self.y > VIRTUAL_HEIGHT - self.height then
         self.y = VIRTUAL_HEIGHT - self.height
         self.vy = -self.vy
     end
+
+    -- Updating ball's position
     self.x = self.x + self.vx * dt
     self.y = self.y + self.vy * dt
+
+end
+
+-- Function to check whether ball has collided with paddle
+function Ball:hasCollided(paddle)
+
+    -- If the horizontal edges of ball and paddle are away from each other
+    -- completely
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+      return false
+    end
+
+    -- If the vertical edges of ball and paddle are away from each other
+    -- completely
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+      return false
+    end
+
+    -- Else there is a collision
+    return true
+
+end
+
+-- Function to update ball on collision with paddle
+function Ball:updateOnCollision(player, paddle)
+
+    -- Scaling of ball's vy velocity depending upon collision
+    -- distance from the center of the paddle
+    yOffsetVelocity = 1 + ( math.abs(self.y - paddle.centerY) / paddle.height ) / 2
+
+    self.vy = self.vy * yOffsetVelocity
+    self.vx = -self.vx * 1.05
+
+    -- For left player
+    if player == "left" then
+        -- Setting ball's position to avoid infinite collision
+        self.x = paddle.x + paddle.width
+
+    -- For right player
+    elseif player == "right" then
+        -- Setting ball's position to avoid infinite collision
+        self.x = paddle.x - self.width
+
+    end
+
+end
+
+-- Function to reset ball
+function Ball:reset()
+
+    -- Resetting required variables
+
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+
+    self.vx = math.random() > 0.5 and 150 or -150
+    self.vy = math.random(-300, 300)
+
 end
