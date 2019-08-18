19df0e1e83543ef7ae378bd4273395e0ffc3cab0
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-18 15:26:50 +0530

    Start State pending/

---
+++ States/StartState.lua
@@ -0,0 +1,8 @@
+StartState = Class{ __includes = BaseState }
+
+-- function StartState:init()
+--     -- Resetting ball, left and right player
+--     ball:reset()
+--     leftPlayer:reset(10, 30)
+--     rightPlayer:reset(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50)
+-- end
