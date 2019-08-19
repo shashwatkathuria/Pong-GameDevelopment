9dcc1414f5509afd6393dc85874c04cf2520caa3
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-19 07:06:07 +0530

    Added end state.

---
+++ States/EndState.lua
@@ -0,0 +1,29 @@
+EndState = Class{ __includes = BaseState }
+
+function EndState:init()
+
+end
+
+function EndState:update(dt)
+    if love.keyboard.wasPressed('return') then
+        stateMachine:change('start')
+
+        leftPlayerScore = 0
+        rightPlayerScore = 0
+        winner = nil
+        
+    end
+end
+
+function EndState:render()
+
+      -- Printing messages specific to end state
+      love.graphics.clear(104 / 255, 118 / 255, 140 / 255, 255 / 255)
+      love.graphics.setColor(224 / 255, 201 / 255, 70 / 255, 255 / 255)
+      love.graphics.setFont(bigFont)
+      love.graphics.printf('GAME END', 0, VIRTUAL_HEIGHT / 2 - 30, VIRTUAL_WIDTH, 'center')
+      love.graphics.setFont(smallFont)
+      love.graphics.printf(winner .. " Player wins the game!", 0, VIRTUAL_HEIGHT / 2 + 30, VIRTUAL_WIDTH, 'center')
+      love.graphics.printf('Press Enter for Rematch', 0, VIRTUAL_HEIGHT - 24, VIRTUAL_WIDTH, 'center')
+
+end
