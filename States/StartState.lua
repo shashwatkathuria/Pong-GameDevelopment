d192fb2e4868f1a7d51414eafeee777eb1aa8cc7
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-22 17:55:21 +0530

    Written start state over from main.lua and also added comments and indentation.

--- States/StartState.lua
+++ States/StartState.lua
@@ -1,8 +1,21 @@
 StartState = Class{ __includes = BaseState }
 
--- function StartState:init()
---     -- Resetting ball, left and right player
---     ball:reset()
---     leftPlayer:reset(10, 30)
---     rightPlayer:reset(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50)
--- end
+function StartState:update(dt)
+
+    -- Changing state to countdown when space is pressed
+    if love.keyboard.wasPressed('space') then
+        stateMachine:change('countdown')
+    end
+
+end
+
+function StartState:render()
+
+    -- Printing messages specific to start state
+    love.graphics.setColor(224 / 255, 201 / 255, 70 / 255, 255 / 255)
+    love.graphics.setFont(bigFont)
+    love.graphics.printf('WELCOME  TO  PONG', 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, 'center')
+    love.graphics.setFont(smallFont)
+    love.graphics.printf('Press Space to Start', 0, VIRTUAL_HEIGHT - 24, VIRTUAL_WIDTH, 'center')
+    
+end
